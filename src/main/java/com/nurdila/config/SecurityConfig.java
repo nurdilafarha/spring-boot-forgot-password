4b63cee4d73ff84dde51d621bc3421cc781b1cfb
nurdilafarha <nurdilafarha@gmail.com>
2022-04-24 23:14:59 +0700

    Add configure in SecurityConfig

--- src/main/java/com/nurdila/config/SecurityConfig.java
+++ src/main/java/com/nurdila/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.nurdila.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -10,4 +11,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
 }
