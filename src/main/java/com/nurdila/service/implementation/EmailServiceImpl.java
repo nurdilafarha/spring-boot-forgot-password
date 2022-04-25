4375336155ba70888804f14dc743ec852a0f355a
nurdilafarha <nurdilafarha@gmail.com>
2022-04-25 11:57:07 +0700

    Add EmailServiceImpl

---
+++ src/main/java/com/nurdila/service/implementation/EmailServiceImpl.java
@@ -0,0 +1,20 @@
+package com.nurdila.service.implementation;
+
+import com.nurdila.service.framework.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    @Autowired
+    public EmailServiceImpl(JavaMailSender mailSender, SpringTemplateEngine templateEngine) {
+        this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
+    }
+
+}
