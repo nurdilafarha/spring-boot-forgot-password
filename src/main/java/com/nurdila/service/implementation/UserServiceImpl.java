1a1177569bac0f4173b8641d6e38e5a3e41c0352
nurdilafarha <nurdilafarha@gmail.com>
2022-04-25 18:13:25 +0700

    Add UserServiceImpl

---
+++ src/main/java/com/nurdila/service/implementation/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.nurdila.service.implementation;
+
+import com.nurdila.repository.RoleRepository;
+import com.nurdila.repository.UserRepository;
+import com.nurdila.service.framework.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder,
+                           RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+    }
+}
