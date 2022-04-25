be908876033d518f8711f7ae2b97799a152fea46
nurdilafarha <nurdilafarha@gmail.com>
2022-04-25 12:13:24 +0700

    Add PasswordResetTokenServiceImpl

---
+++ src/main/java/com/nurdila/service/implementation/PasswordResetTokenServiceImpl.java
@@ -0,0 +1,16 @@
+package com.nurdila.service.implementation;
+
+import com.nurdila.repository.PasswordResetTokenRepository;
+import com.nurdila.service.framework.PasswordResetTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordResetTokenServiceImpl implements PasswordResetTokenService {
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+
+    @Autowired
+    public PasswordResetTokenServiceImpl(PasswordResetTokenRepository passwordResetTokenRepository) {
+        this.passwordResetTokenRepository = passwordResetTokenRepository;
+    }
+}
