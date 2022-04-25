61e9b8a5c0c1324e9461144acde2cbf4c87ea670
nurdilafarha <nurdilafarha@gmail.com>
2022-04-25 11:41:30 +0700

    Add PasswordResetTokenService

---
+++ src/main/java/com/nurdila/service/framework/PasswordResetTokenService.java
@@ -0,0 +1,9 @@
+package com.nurdila.service.framework;
+
+import com.nurdila.entity.PasswordResetToken;
+
+public interface PasswordResetTokenService {
+    PasswordResetToken findByToken(String token);
+
+    PasswordResetToken save(PasswordResetToken passwordResetToken);
+}
