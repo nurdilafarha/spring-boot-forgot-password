581f6c31c7d1f6082d8f01516e47a47fe7b16100
nurdilafarha <nurdilafarha@gmail.com>
2022-04-25 12:06:18 +0700

    Add send in EmailServiceImpl

--- src/main/java/com/nurdila/service/implementation/EmailServiceImpl.java
+++ src/main/java/com/nurdila/service/implementation/EmailServiceImpl.java
@@ -1,10 +1,17 @@
 package com.nurdila.service.implementation;
 
+import com.nurdila.entity.Mail;
 import com.nurdila.service.framework.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -15,6 +22,26 @@ public class EmailServiceImpl implements EmailService {
     public EmailServiceImpl(JavaMailSender mailSender, SpringTemplateEngine templateEngine) {
         this.mailSender = mailSender;
         this.templateEngine = templateEngine;
+    }
+
+    @Override
+    public void send(Mail mail) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,
+                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name());
+            Context context = new Context();
+            context.setVariables(mail.getModel());
+            String html = templateEngine.process("email/email-template", context);
+            helper.setTo(mail.getTo());
+            helper.setFrom(mail.getFrom());
+            helper.setSubject(mail.getSubject());
+            helper.setText(html, true);
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
 }
