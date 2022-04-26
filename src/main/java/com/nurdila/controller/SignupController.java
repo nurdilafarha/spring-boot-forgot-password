7cbce32e0075cd0bb8ed98e80c466a31fda55413
nurdilafarha <nurdilafarha@gmail.com>
2022-04-26 10:50:24 +0700

    Add SignupController

---
+++ src/main/java/com/nurdila/controller/SignupController.java
@@ -0,0 +1,20 @@
+package com.nurdila.controller;
+
+import com.nurdila.service.framework.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class SignupController {
+    private final UserService userService;
+    private final MessageSource messageSource;
+
+    @Autowired
+    public SignupController(UserService userService, MessageSource messageSource) {
+        this.userService = userService;
+        this.messageSource = messageSource;
+    }
+}
